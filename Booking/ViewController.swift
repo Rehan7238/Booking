43a11b8a36714e16fdbb5f258ae2c41d23c2ad66
Rehan Name <rehan.chaudhry7238@gmail.com>
2020-06-07 20:13:07 -0400

    First commit, just setting up :)

---
+++ Booking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Booking
+//
+//  Created by Rehan Chaudhry on 6/7/20.
+//  Copyright © 2020 Rehan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookingTests/BookingTests.swift
@@ -0,0 +1,34 @@
+//
+//  BookingTests.swift
+//  BookingTests
+//
+//  Created by Rehan Chaudhry on 6/7/20.
+//  Copyright © 2020 Rehan. All rights reserved.
+//
+
+import XCTest
+@testable import Booking
+
+class BookingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
