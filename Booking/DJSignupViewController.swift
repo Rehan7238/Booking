e88aaf448b371621c1b03bd189fe7d1966f651c5
Rehan Name <rehan.chaudhry7238@gmail.com>
2020-06-10 19:12:32 -0400

    terrible signup screen

---
+++ Booking/DJSignupViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DJSignupViewController.swift
+//  Booking
+//
+//  Created by Rehan Chaudhry on 6/10/20.
+//  Copyright © 2020 Rehan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DJSignupViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
