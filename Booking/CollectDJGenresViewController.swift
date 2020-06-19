2eb26d1a39a5ffd93f146c5b1151c9b760e86731
Eimara Mirza <em5nu@virginia.edu>
2020-06-19 00:33:34 -0400

    Added All Initial DJ Sign Up Flow

---
+++ Booking/CollectDJGenresViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectDJGenresViewController.swift
+//  Booking
+//
+//  Created by Eimara Mirza on 6/19/20.
+//  Copyright © 2020 Rehan. All rights reserved.
+//
+
+import Foundation
---
+++ Booking/CollectDJLinkViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectDJLinkViewController.swift
+//  Booking
+//
+//  Created by Eimara Mirza on 6/19/20.
+//  Copyright © 2020 Rehan. All rights reserved.
+//
+
+import Foundation
