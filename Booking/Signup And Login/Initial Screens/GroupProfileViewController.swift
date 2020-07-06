58954a4d869cf7faf8adb51efaf6ec6cad0c1a5b
Eimara Mirza <em5nu@virginia.edu>
2020-07-05 22:27:21 -0400

    small changes

---
+++ Booking/Signup And Login/Initial Screens/GroupProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GroupProfileViewController.swift
+//  Booking
+//
+//  Created by Eimara Mirza on 7/5/20.
+//  Copyright © 2020 Rehan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FSCalendar
+import FirebaseAuth
+
+class GroupProfileViewController: UIViewController{
+    
+    //Mark: Properties
+    
+    var group: Group?
+    
+    @IBOutlet weak var groupName: UILabel!
+    @IBOutlet weak var profilePic: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let uid = Auth.auth().currentUser?.uid {
+                  _ = Group.fromID(id: uid).done { loadedGroup in
+                      self.group = loadedGroup
+                    self.groupName.text = self.group?.name
+                  }
+              }
+        
+        
+    }
+}
