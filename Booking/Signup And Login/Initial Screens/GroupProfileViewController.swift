529392efc7deb07fad1018ccecf559a3dd48a842
Eimara Mirza <em5nu@virginia.edu>
2020-07-13 15:14:15 -0400

    added tableview to profile for later use with requests

--- Booking/Signup And Login/Initial Screens/GroupProfileViewController.swift
+++ Booking/Signup And Login/Initial Screens/GroupProfileViewController.swift
@@ -14,7 +14,7 @@ import Firebase
 import FirebaseStorage
 import GooglePlaces
 
-class GroupProfileViewController: UIViewController {
+class GroupProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //Mark: Properties
     
@@ -25,22 +25,32 @@ class GroupProfileViewController: UIViewController {
     @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var schoolLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    var results: [String] = [String]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        profilePic.layer.cornerRadius = profilePic.layer.bounds.height / 2
+        profilePic.layer.cornerRadius = profilePic.layer.bounds.height / 6
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         
         if let uid = Auth.auth().currentUser?.uid {
             _ = Group.fromID(id: uid).done { loadedGroup in
                 self.group = loadedGroup
+                self.refreshData()
                 self.groupName.text = self.group?.name
                 self.addressLabel.text = self.group?.address
                 self.schoolLabel.text = self.group?.school
                 if let profilePic = self.group?.profilePic, !profilePic.isEmpty {
                     self.profilePic.downloadImage(from: URL(string: profilePic)!)
+                    
                 }
+
             }
         }
     }
@@ -50,18 +60,52 @@ class GroupProfileViewController: UIViewController {
         
         profilePic.layer.cornerRadius = profilePic.layer.bounds.height / 6
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         
         if let uid = Auth.auth().currentUser?.uid {
             _ = Group.fromID(id: uid).done { loadedGroup in
                 self.group = loadedGroup
+                self.refreshData()
                 self.groupName.text = self.group?.name
                 self.addressLabel.text = self.group?.address
                 self.schoolLabel.text = self.group?.school
                 if let profilePicURL = self.group?.profilePic, let url = URL(string: profilePicURL) {
                     self.profilePic.downloadImage(from: url)
+
                 }
+
             }
         }
+    }
+    
+    func refreshData() {
+           let db = Firestore.firestore()
+
+           db.collection("Events").whereField("school", isEqualTo: group?.school ?? "").getDocuments() { (querySnapshot, err) in
+               if let err = err {
+                   print("Error getting documents: \(err)")
+               } else {
+                   self.results = [String]()
+                for document in querySnapshot!.documents {
+                        self.results.append(document.documentID)
+                }
+                   self.tableView.reloadData()
+               }
+           }
+       }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell") as! EventCell
+        let id = results[indexPath.row]
+        cell.setup(eventID: id)
+        cell.layer.cornerRadius = cell.frame.height / 3
+        return cell
     }
 }
 
