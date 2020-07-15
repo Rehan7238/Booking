6ffce339699b5dc260d93552f186bf63d079765c
Rehan Name <rehan.chaudhry7238@gmail.com>
2020-07-15 15:56:05 -0400

    buf

--- Booking/DJProfileViewController.swift
+++ Booking/DJProfileViewController.swift
@@ -23,21 +23,21 @@ class DJProfileViewController: UIViewController, UITableViewDelegate, UITableVie
     @IBOutlet weak var DJRatingNumber: UILabel!
     @IBOutlet weak var numberOfGigsNumber: UILabel!
     @IBOutlet weak var tableView: UITableView!
-
+    
     
     var dj: DJ?
     var request: Request?
     var results: [String] = [String]()
-
-        
     
-override func viewDidLoad() {
+    
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         
         profilePic.layer.cornerRadius = profilePic.layer.bounds.height / 2
-    
-    tableView.delegate = self
-    tableView.dataSource = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         
         
         if let uid = Auth.auth().currentUser?.uid {
@@ -47,11 +47,17 @@ override func viewDidLoad() {
                 if let profilePic = self.dj?.profilePic {
                     self.profilePic.downloadImage(from: URL(string: profilePic)!)
                     var calculatedRating = 0
-                    let ratings = self.dj?.hostRating
-                    for rating in ratings!{
-                        calculatedRating = calculatedRating + Int(truncating: rating)
+                    if let ratings = self.dj?.hostRating {
+                        for rating in ratings {
+                            calculatedRating = calculatedRating + Int(truncating: rating)
+                        }
+                        
+                        if ratings.count == 0 {
+                            self.DJRatingNumber.text = "0.0"
+                        } else {
+                            self.DJRatingNumber.text = "\(String(describing: calculatedRating/ratings.count))"
+                        }
                     }
-                    self.DJRatingNumber.text = "\(String(describing: calculatedRating/ratings!.count))"
                     self.numberOfGigsNumber.text = "\(String(describing: self.dj?.numberOfGigs ?? 0))"
                 }
             }
@@ -61,25 +67,30 @@ override func viewDidLoad() {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-               profilePic.layer.cornerRadius = profilePic.layer.bounds.height / 6
-               
-               
-               if let uid = Auth.auth().currentUser?.uid {
-                   _ = DJ.fromID(id: uid).done { loadedDJ in
-                       self.dj = loadedDJ
-                       self.djName.text = self.dj?.name
-                       if let profilePic = self.dj?.profilePic {
-                           self.profilePic.downloadImage(from: URL(string: profilePic)!)
-                        var calculatedRating = 0
-                        let ratings = self.dj?.hostRating
-                        for rating in ratings!{
+        profilePic.layer.cornerRadius = profilePic.layer.bounds.height / 6
+        
+        
+        if let uid = Auth.auth().currentUser?.uid {
+            _ = DJ.fromID(id: uid).done { loadedDJ in
+                self.dj = loadedDJ
+                self.djName.text = self.dj?.name
+                if let profilePic = self.dj?.profilePic {
+                    self.profilePic.downloadImage(from: URL(string: profilePic)!)
+                    var calculatedRating = 0
+                    if let ratings = self.dj?.hostRating {
+                        for rating in ratings {
                             calculatedRating = calculatedRating + Int(truncating: rating)
                         }
-                        self.DJRatingNumber.text = "\(String(describing: calculatedRating/ratings!.count))"
+                        if ratings.count == 0 {
+                            self.DJRatingNumber.text = "0.0"
+                        } else {
+                            self.DJRatingNumber.text = "\(String(describing: calculatedRating/ratings.count))"
+                        }
                         self.numberOfGigsNumber.text = "\(String(describing: self.dj?.numberOfGigs ?? 0))"
-                       }
-                   }
-               }
+                    }
+                }
+            }
+        }
     }
     
     @IBAction func InstagramAction() {
@@ -89,7 +100,7 @@ override func viewDidLoad() {
             let appURL = URL(string: "instagram://user?username=" + (username))!
             
             let application = UIApplication.shared
-
+            
             if application.canOpenURL(appURL) {
                 application.open(appURL)
             } else {
@@ -98,20 +109,20 @@ override func viewDidLoad() {
                 application.open(webURL)
             }
         }
-
+        
     }
     
     func refreshData() {
         let db = Firestore.firestore()
-
+        
         db.collection("Requests").whereField("DJID", isEqualTo: dj?.id ?? "").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
                 self.results = [String]()
-             for document in querySnapshot!.documents {
-                     self.results.append(document.documentID)
-             }
+                for document in querySnapshot!.documents {
+                    self.results.append(document.documentID)
+                }
                 self.tableView.reloadData()
             }
         }
@@ -130,42 +141,42 @@ override func viewDidLoad() {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let selectedRequestid = results[indexPath.row]
-            print (selectedRequestid)
-            // show different view controllers based on different cases
-            _ = Request.fromID(id: selectedRequestid).done { loadedRequest in
-                self.request = loadedRequest
-                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusOpenDJ", owner: nil, options: nil)?.first as? checkRequestStatusOpenDJ {
+        let selectedRequestid = results[indexPath.row]
+        print (selectedRequestid)
+        // show different view controllers based on different cases
+        _ = Request.fromID(id: selectedRequestid).done { loadedRequest in
+            self.request = loadedRequest
+            if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusOpenDJ", owner: nil, options: nil)?.first as? checkRequestStatusOpenDJ {
                 showRequestVC.DJparentView = self
                 showRequestVC.uid = selectedRequestid
                 self.present(showRequestVC, animated: true, completion: nil)
-                }
-                
-                // if the status is open
-    //            if loadedRequest?.status == "open" {
-    //                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusOpen", owner: nil, options: nil)?.first as? checkRequestStatusOpen {
-    //                showRequestVC.DJparentView = self
-    //                showRequestVC.uid = selectedRequestid
-    //                self.present(showRequestVC, animated: true, completion: nil)
-    //                }
-    //            }
-    //            // if the status was declined
-    //            else if loadedRequest?.status == "declined" {
-    //                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusDeclined", owner: nil, options: nil)?.first as? checkRequestStatusDeclined {
-    //                showRequestVC.DJparentView = self
-    //                showRequestVC.uid = selectedRequestid
-    //                self.present(showRequestVC, animated: true, completion: nil)
-    //                }
-    //            }
-    //            // if the status was countered
-    //            else if loadedRequest?.status == "countered" {
-    //                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusCounter", owner: nil, options: nil)?.first as? checkRequestStatusCounter {
-    //                showRequestVC.DJparentView = self
-    //                showRequestVC.uid = selectedRequestid
-    //                self.present(showRequestVC, animated: true, completion: nil)
-    //                }
-    //            }
-    
+            }
+            
+            // if the status is open
+            //            if loadedRequest?.status == "open" {
+            //                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusOpen", owner: nil, options: nil)?.first as? checkRequestStatusOpen {
+            //                showRequestVC.DJparentView = self
+            //                showRequestVC.uid = selectedRequestid
+            //                self.present(showRequestVC, animated: true, completion: nil)
+            //                }
+            //            }
+            //            // if the status was declined
+            //            else if loadedRequest?.status == "declined" {
+            //                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusDeclined", owner: nil, options: nil)?.first as? checkRequestStatusDeclined {
+            //                showRequestVC.DJparentView = self
+            //                showRequestVC.uid = selectedRequestid
+            //                self.present(showRequestVC, animated: true, completion: nil)
+            //                }
+            //            }
+            //            // if the status was countered
+            //            else if loadedRequest?.status == "countered" {
+            //                if let showRequestVC = Bundle.main.loadNibNamed("checkRequestStatusCounter", owner: nil, options: nil)?.first as? checkRequestStatusCounter {
+            //                showRequestVC.DJparentView = self
+            //                showRequestVC.uid = selectedRequestid
+            //                self.present(showRequestVC, animated: true, completion: nil)
+            //                }
+            //            }
+            
         }
     }
 }
